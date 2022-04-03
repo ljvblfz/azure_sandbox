905c52c5845edea76a64f84b5e512e5bcd4044a4
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-04-04 02:16:07 +0700

    Update az-create-plus.sh

--- katacoda/AZ/az-create-plus.sh
+++ katacoda/AZ/az-create-plus.sh
@@ -226,7 +226,7 @@ do
             [nN][oO]|[nN])
                   echo "🖥️  Deleting VM... (about 3m)"
                   rs=$(cat rs) 
-                  az vm delete --ids $(az vm list -g $rs --query "[].id" -o tsv) --yes
+                  #az vm delete --ids $(az vm list -g $rs --query "[].id" -o tsv) --yes
                   app=$(az appservice plan list --query "[].name" -o tsv)
                   web=$(az webapp list --query "[].repositorySiteName" --output tsv)
                   az webapp delete --name $web --resource-group $rs 
