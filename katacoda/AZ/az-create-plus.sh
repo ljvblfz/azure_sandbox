6ce925e515c691dd00c5f0a8acfe3079ff884531
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-04-03 23:27:38 +0700

    Update az-create-plus.sh

--- katacoda/AZ/az-create-plus.sh
+++ katacoda/AZ/az-create-plus.sh
@@ -4,7 +4,28 @@ stty quit ""
 stty susp undef
 
 
+cat <<EOF >>spinner.sh
+#!/usr/bin/env bash
 
+show_spinner()
+{
+  local -r pid="${1}"
+  local -r delay='0.1'
+  local spinstr='\|/-'
+  local temp
+  while ps a | awk '{print $1}' | grep -q "${pid}"; do
+    temp="${spinstr#?}"
+    printf " [%c]  " "${spinstr}"
+    spinstr=${temp}${spinstr%"${temp}"}
+    sleep "${delay}"
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
+}
+
+("$@") &
+show_spinner "$!"
+EOF
 
 
 
