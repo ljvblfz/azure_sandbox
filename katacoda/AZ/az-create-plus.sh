08056c61ef097274a3dee2856cd4a585128eb5c3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-04-04 00:05:26 +0700

    Update az-create-plus.sh

--- katacoda/AZ/az-create-plus.sh
+++ katacoda/AZ/az-create-plus.sh
@@ -246,6 +246,7 @@ do
                   rs=$(cat rs) 
                   az vm delete --ids $(az vm list -g $rs --query "[].id" -o tsv)
                   app=$(az appservice plan list --query "[].name" -o tsv)
+                  az webapp delete --name haivm898128969898128969 --resource-group $rs
                   az appservice plan delete --name $app --resource-group $rs --yes
                   bash stop
                   goto begin
