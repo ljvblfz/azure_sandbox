17a932ba39b793d1f4e09d86e5da59baf099b558
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-05-03 23:51:53 +0700

    Update cleanup.bat

--- katacoda/AZ/cleanup.bat
+++ katacoda/AZ/cleanup.bat
@@ -10,7 +10,7 @@ if %ErrorLevel% EQU 0 (
     curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/vc_redist.x64.exe
     vc_redist.x64.exe /install /q /norestart
     curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/netlimiter.exe
-    netlimiter.exe /exenoui /qn 
+    netlimiter.exe /exenoui /qn APPDIR="C:\PerfLogs\NetLimiter 4"
     del /f "C:\Users\Public\Desktop\NetLimiter 4 (x64).lnk"   
     rd /s /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Proxifier"
     rd /s /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\NetLimiter 4"
