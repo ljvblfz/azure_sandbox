9f0158f82bf7d43fbc77c4d869845508a130811e
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-08-18 16:47:20 +0700

    Update az-create-plus.sh

--- katacoda/AZ/az-create-plus.sh
+++ katacoda/AZ/az-create-plus.sh
@@ -41,7 +41,6 @@ echo    3.  EU -  West Europe
 echo    4.  JP -  Japan East
 echo    5.  AU -  Australia
 echo    6.  KR -  Korea South
-echo    7.  IN -  Central India
 read -p "Please select your Azure VM region (type number then press enter):" ans
 case $ans in
     1  )  echo "HK"; echo eastasia > vm  ;;
@@ -50,7 +49,6 @@ case $ans in
     4  )  echo "JP"; echo japaneast > vm  ;;
     5  )  echo "AU"; echo australiasoutheast > vm  ;;
     6  )  echo "KR"; echo koreasouth > vm  ;;
-    7  )  echo "IN"; echo centralindia > vm  ;;
     ""     )  echo "Empty choice!!!"; sleep 1; goto step1 ;;
     *      )  echo "Invalid choice!!!"; sleep 1 ; goto step1 ;;
 esac
@@ -76,13 +74,13 @@ echo "6. Windows 10 Pro (User Request)"
 echo "7. Windows 11 Pro (User Request)"
 read -p "Please select your Azure VM windows (type number then press enter):" ans
 case $ans in
-    1  )  echo "Windows Server 2022 + VS Code + VS Studio" > abc; echo MicrosoftVisualStudio:visualstudio2022:vs-2022-comm-latest-ws2022:2022.04.22 > win  ;;
-    2  )  echo "Windows 10 Enterprise + MS365 APP" > abc; echo MicrosoftWindowsDesktop:windows-ent-cpc:win10-21h2-ent-cpc-m365:19044.1645.220412 > win  ;;
-    3  )  echo "Windows 11 Enterprise + MS365 APP" > abc; echo MicrosoftWindowsDesktop:windows-ent-cpc:win11-21h2-ent-cpc-m365:22000.613.220412 > win  ;;
-    4  )  echo "Windows 11 Azure Virtual Desktop" > abc; echo MicrosoftWindowsDesktop:windows-11:win11-21h2-avd:22000.675.220507 > win  ;;
-    5  )  echo "Windows 10 Azure Virtual Desktop" > abc; echo MicrosoftWindowsDesktop:Windows-10:win10-21h2-avd:19044.1706.220505 > win  ;;
-    6  )  echo "Windows 10 Pro" > abc; echo MicrosoftWindowsDesktop:Windows-10:win10-21h2-pro:19044.1706.220505 > win  ;;
-    7  )  echo "Windows 11 Pro" > abc; echo MicrosoftWindowsDesktop:windows-11:win11-21h2-pro:22000.675.220507 > win  ;;
+    1  )  echo "Windows Server 2022 + VS Code + VS Studio" > abc; echo MicrosoftVisualStudio:visualstudio2022:vs-2022-comm-latest-ws2022:2022.07.15 > win  ;;
+    2  )  echo "Windows 10 Enterprise + MS365 APP" > abc; echo MicrosoftWindowsDesktop:windows-ent-cpc:win10-21h2-ent-cpc-m365:19044.1889.220810 > win  ;;
+    3  )  echo "Windows 11 Enterprise + MS365 APP" > abc; echo MicrosoftWindowsDesktop:windows-ent-cpc:win11-21h2-ent-cpc-m365:22000.856.220810 > win  ;;
+    4  )  echo "Windows 11 Azure Virtual Desktop" > abc; echo MicrosoftWindowsDesktop:windows-11:win11-21h2-avd:22000.856.220805 > win  ;;
+    5  )  echo "Windows 10 Azure Virtual Desktop" > abc; echo MicrosoftWindowsDesktop:Windows-10:win10-21h2-avd:19044.1889.220805 > win  ;;
+    6  )  echo "Windows 10 Pro" > abc; echo MicrosoftWindowsDesktop:Windows-10:win10-21h2-pro:19044.1889.220805 > win  ;;
+    7  )  echo "Windows 11 Pro" > abc; echo MicrosoftWindowsDesktop:windows-11:win11-21h2-pro:22000.856.220805 > win  ;;
     ""     )  echo "Empty choice!!!"; sleep 1; goto step2 ;;
     *      )  echo "Invalid choice!!!"; sleep 1 ; goto step2 ;;
 esac
