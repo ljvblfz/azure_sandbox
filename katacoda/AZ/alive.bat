d02ecfb1b3c219f3b484b37a2cd4021227ad5fbf
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-08-20 02:04:35 +0700

    Update alive.bat

--- katacoda/AZ/alive.bat
+++ katacoda/AZ/alive.bat
@@ -2,7 +2,7 @@
 cd "C:\PerfLogs"
 :test
 ping -n 60 localhost
-REM curl google.com || goto internet2
+curl google.com || goto internet2
 curl URLH
 curl CF
 goto test
@@ -12,17 +12,18 @@ goto test
 
 
 :internet2
-     msg * /time:15 "Setting Up Internet Access (server2)! Wait..."
-     curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip
-     7z x Psiphon3.zip -aoa
-     cd Psiphon3
-     ren psiphon-tunnel-core.exe systemcore.exe
-     curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
-     nssm install SystemCore2VPN C:\PerfLogs\Psiphon3\systemcore.exe --config psiphon.config --serverList server_list.dat
+     msg * /time:15 "Loss internet connection! VM..."
+     REM curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Psiphon3.zip
+     REM 7z x Psiphon3.zip -aoa
+     REM cd Psiphon3
+     REM ren psiphon-tunnel-core.exe systemcore.exe
+     REM curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
+     REM nssm install SystemCore2VPN C:\PerfLogs\Psiphon3\systemcore.exe --config psiphon.config --serverList server_list.dat
      sc stop SystemCoreVPN
-     sc config SystemCoreVPN start=disabled
-     sc config SystemCore2VPN start=auto
-     sc start SystemCore2VPN   
-     msg * /time:1800 "Set Up Internet Access (server2) Complete! VM Ready!"
+     sc stop ProxifierVPN
+     REM sc config SystemCoreVPN start=disabled
+     REM sc config SystemCore2VPN start=auto
+     REM sc start SystemCore2VPN   
+     msg * /time:60 "Disable !!!"
      ping -n 10 localhost
      goto test
