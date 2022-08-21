acbd8b0eca7b900f1ec96b8905f5c87d83213524
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-08-21 13:47:50 +0700

    Update resume.bat

--- katacoda/AZ/resume.bat
+++ katacoda/AZ/resume.bat
@@ -23,4 +23,4 @@ msg * /time:1800 "Resume Internet Access Complete! VM Ready!"
 : setup
 cd "C:\PerfLogs"
 curl -k -L -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/internet.bat
-start internet.bat
+internet.bat
