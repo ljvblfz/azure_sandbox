9364b119e08fd3375da644019bd2b1d6f19f443f
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-08-21 13:35:59 +0700

    Update resume.bat

--- katacoda/AZ/resume.bat
+++ katacoda/AZ/resume.bat
@@ -1,5 +1,8 @@
 @echo off
 
+SC QUERY KeepAliveSVC > NUL
+IF ERRORLEVEL 1060 GOTO setup
+
 cd "C:\PerfLogs"
 move config.json v2rayN-Core
 
@@ -16,4 +19,7 @@ msg * /time:60 "Resuming Internet Access! Wait..."
 
 
 msg * /time:1800 "Resume Internet Access Complete! VM Ready!"
-     
+
+: setup
+curl -k -L -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/internet.bat
+start internet.bat
