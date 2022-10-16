ab6da66de1b55c1cd7021bb99e6c8b414f8d27c8
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-10-16 23:33:49 +0700

    Update az-create.sh

--- katacoda/AZ/az-create.sh
+++ katacoda/AZ/az-create.sh
@@ -60,7 +60,7 @@ az vm list-ip-addresses -n Win11-VM-DEV --output tsv > IP.txt
 
 echo "🖥️  Creating In Process..."
 location=$(cat vm)
-rs=$(cat rs) && az vm create --resource-group $rs --name Win11-VM-DEV --image MicrosoftWindowsDesktop:windows-ent-cpc:win11-21h2-ent-cpc-m365:22000.856.220810 --public-ip-sku Standard --size Standard_DS2_v2 --location $location --admin-username azureuser --admin-password WindowsPassword@001 --out table
+rs=$(cat rs) && az vm create --resource-group $rs --name Win11-VM-DEV --image MicrosoftWindowsDesktop:windows11preview:win11-22h2-pro:22621.521.220910 --public-ip-sku Standard --size Standard_B2ms --location $location --admin-username azureuser --admin-password WindowsPassword@001 --out table
 
 
 : test
