77b7ac68bb6199e12689829c4e2012f595cc3659
demo <58414694+kmille36@users.noreply.github.com>
2022-12-21 13:28:05 +0800

    测试

--- katacoda/AZ/az-create-plus.sh
+++ katacoda/AZ/az-create-plus.sh
@@ -111,10 +111,10 @@ NUMBER=$(cat number)
 echo "haivm$NUMBER$NUMBER.azurewebsites.net/metrics" > site
 
 location=$(cat vm)
-echo "az appservice plan create --name myAppServicePlan$NUMBER$NUMBER --resource-group $rs --location $location --sku F1 --is-linux --output none && az webapp create --resource-group $rs --plan myAppServicePlan$NUMBER$NUMBER --name haivm$NUMBER$NUMBER --deployment-container-image-name docker.io/thuonghai2711/v2ray-azure-web:latest --output none" > webapp.sh 
+echo "az appservice plan create --name myAppServicePlan$NUMBER$NUMBER --resource-group $rs --location $location --sku F1 --is-linux --output none && az webapp create --resource-group $rs --plan myAppServicePlan$NUMBER$NUMBER --name haivm$NUMBER$NUMBER --deployment-container-image-name docker.io/thuonghai2711/v2ray-azure-web:latest --output none" > webapp.sh
 nohup bash webapp.sh  &>/dev/null &
 
-echo "Windows-${RANDOM}" > VirtualMachineName.txt
+echo "Windows-${RANDOM}" >> VirtualMachineName.txt
 
 goto checkvm
 : checkvm
